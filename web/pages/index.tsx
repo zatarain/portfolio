d00aa0d3ae65390c72df35271c51d38675ae2f06
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-04-17 21:24:44 +0100

    PORT-002: Adding override for index.tsx
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

---
+++ web/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const IndexPage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>CV - Ulises Tirado Zatarain</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        <h1>Hello World!</h1>
+        <p>It works!</p>
+      </header>
+    </>
+  )
+}
+
+export default IndexPage
