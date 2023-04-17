9b10376bb339450a6461f73ea84931d64453fd35
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-04-17 22:29:13 +0100

    PORT-001: Adding tests for homepage
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

---
+++ web/pages/index.spec.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+import IndexPage from '.'
+
+describe('<IndexPage />', () => {
+	it('renders the home page', () => {
+
+		render(<IndexPage />)
+
+		expect(screen.getByText('It works!')).toBeInTheDocument()
+		expect(screen.getByText('Hello World!')).toBeInTheDocument()
+	})
+})
