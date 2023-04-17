25525889a133760aaac96d8be63077a0a66a2eb2
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-04-17 19:38:53 +0100

    PORT-002: Fixing rubocop offenses
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- api/app/controllers/experiences_controller.rb
+++ api/app/controllers/experiences_controller.rb
@@ -1,38 +1,40 @@
+# frozen_string_literal: true
+
 class ExperiencesController < ApplicationController
-	def index
-		experiences = [
-			{
-				id: 1,
-				title: 'Software Engineer',
-				company: 'Deliveroo',
-				city: 'London',
-				country: 'UK',
-				type: 'full-time',
-				from: '13/09/2021',
-				to: nil,
-				description: 'Hello world 1!',
-			},
-			{
-				id: 2,
-				title: 'Software Engineer',
-				company: 'Goldman Sachs',
-				city: 'London',
-				country: 'UK',
-				type: 'full-time',
-				from: '04/02/2019',
-				to: '01/09/2021',
-				description: 'Hello world 2!',
-			},
-		]
-	
-		if experiences
-			render json: {
-				status: "SUCCESS",
-				message: "Work experiences successfully fetched.",
-				data: experiences,
-			}, status: :ok
-		else
-			render json: friends.errors, status: :bad_request
-		end
-	end
+  def index
+    experiences = [
+      {
+        id: 1,
+        title: 'Software Engineer',
+        company: 'Deliveroo',
+        city: 'London',
+        country: 'UK',
+        type: 'full-time',
+        from: '13/09/2021',
+        to: nil,
+        description: 'Hello world 1!'
+      },
+      {
+        id: 2,
+        title: 'Software Engineer',
+        company: 'Goldman Sachs',
+        city: 'London',
+        country: 'UK',
+        type: 'full-time',
+        from: '04/02/2019',
+        to: '01/09/2021',
+        description: 'Hello world 2!'
+      }
+    ]
+
+    if experiences
+      render json: {
+        status: 'SUCCESS',
+        message: 'Work experiences successfully fetched.',
+        data: experiences
+      }, status: :ok
+    else
+      render json: friends.errors, status: :bad_request
+    end
+  end
 end
--- api/app/controllers/pages_controller.rb
+++ api/app/controllers/pages_controller.rb
@@ -1,41 +1,31 @@
+# frozen_string_literal: true
+
 class PagesController < ApplicationController
-	def home
-			render json: {
-				status: "SUCCESS",
-				message: "Rendering CV",
-				data: {
-					name: 'Ulises Tirado Zatarain',
-					emails: [
-						'u*****o@gmail.com',
-						'u*****o@cimat.mx',
-					],
-					phones: [
-						'+44 07 (516) XXX XXX',
-						'+52 1 (669) XXX XXXX',
-					],
-					websites: [
-						'https://github.com/zatarain',
-					],
-					statement: '',
-					experiences: [
-
-					],
-					qualifications: [
-
-					],
-					projects: [
-
-					],
-					skills: [
-
-					],
-					awards: [
-
-					],
-					volunteering: [
-
-					]
-				},
-			}, status: :ok
-	end
+  def home
+    render json: {
+      status: 'SUCCESS',
+      message: 'Rendering CV',
+      data: {
+        name: 'Ulises Tirado Zatarain',
+        emails: [
+          'u*****o@gmail.com',
+          'u*****o@cimat.mx'
+        ],
+        phones: [
+          '+44 07 (516) XXX XXX',
+          '+52 1 (669) XXX XXXX'
+        ],
+        websites: [
+          'https://github.com/zatarain'
+        ],
+        statement: '',
+        experiences: [],
+        qualifications: [],
+        projects: [],
+        skills: [],
+        awards: [],
+        volunteering: []
+      }
+    }, status: :ok
+  end
 end
