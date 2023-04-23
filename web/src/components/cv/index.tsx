4959510372277e7d7a3f69998fd9dc5d709e2c92
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-04-23 19:18:07 +0100

    PORT-003: Interface for CV data
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- web/src/components/cv/index.tsx
+++ web/src/components/cv/index.tsx
@@ -1,5 +1,19 @@
 import { useCV } from '#hooks'
 
+export interface CurriculumVitaeProperties {
+	name: string
+	emails?: string[],
+	phones?: string[],
+	websites?: string[],
+	statement?: string,
+	experiences?: string[],
+	qualifications?: string[],
+	projects?: string[],
+	skills?: string[],
+	awards?: string[],
+	volunteering?: string[]
+}
+
 function CurriculumVitae() {
 	const { data } = useCV()
 	return (
