e6fe57d962670f65ad75e570b55543b39582184d
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-06-06 21:12:21 +0100

    PORT-004: Upgrading rubocop and its configuration
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- api/spec/requests/experiences_spec.rb
+++ api/spec/requests/experiences_spec.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'Experiences', type: :request do
   describe 'GET /experiences' do
-    it 'should response HTTP/200 OK with a JSON' do
+    it 'responses with JSON data on body' do
       get '/experiences'
-      expect(response.status).to eq(200)
-      json = JSON.parse(response.body).deep_symbolize_keys
+      json = response.parsed_body.deep_symbolize_keys
       expect(json[:message]).to eq('Work experiences successfully fetched.')
+      expect(response.status).to eq(200)
     end
   end
 end
