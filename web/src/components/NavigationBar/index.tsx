09013c178e23b296c6e8bfad209548ecfb5b85a1
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-06-20 22:06:45 +0100

    PORT-006: Making navigation bar responsive in all resolutions
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- web/src/components/NavigationBar/index.tsx
+++ web/src/components/NavigationBar/index.tsx
@@ -1,28 +1,39 @@
 import Image from 'next/image';
 import Link from 'next/link'
+import styles from './index.module.css'
 
 interface Properties {
 	name: string
 }
 
+function toggleResponsive(event: any) {
+	console.log('Yay! Click!')
+	console.dir(event)
+}
+
 const NavigationBar = ({ name }: Properties) => {
 	return (
-		<nav>
-			<Link href="/" className="logo">
+		<nav className={styles['navigation-bar']}>
+			<Link href="/" className={styles.logo}>
 				<Image alt="logo" src="/logo.svg" width="48" height="48" />
 				{name}
 			</Link>
-			<div className="sections">
-				<Link href="#">Experience</Link>
-				<Link href="#">Education</Link>
-				<Link href="#">Skills</Link>
-				<Link href="#">Projects</Link>
-				<Link href="#">Hobbies</Link>
-			</div>
-			<div className="actions">
-				<Link href="#">E-mail</Link>
-				<Link href="#" className="call-to-action">Download</Link>
-			</div>
+			<ul className={styles.sections}>
+				<li><Link href="#">Experience</Link></li>
+				<li><Link href="#">Education</Link></li>
+				<li><Link href="#">Skills</Link></li>
+				<li><Link href="#">Projects</Link></li>
+				<li><Link href="#">Hobbies</Link></li>
+				<li className={styles.action}><Link href="#">E-mail</Link></li>
+				<li className={styles.action}>
+					<Link href="#" onClick={toggleResponsive} className={styles['call-to-action']}>Download</Link>
+				</li>
+				<li className={styles.icon}>
+					<Link href="#">
+						<i className="fa fa-bars"></i>
+					</Link>
+				</li>
+			</ul>
 		</nav>
 	)
 }
--- web/src/components/PageLayout/index.tsx
+++ web/src/components/PageLayout/index.tsx
@@ -24,6 +24,8 @@ export default function PageLayout({ children, title, data }: Properties) {
 		<div className={`${inter.className} container`}>
 			<Head>
 				<title>{pageTitle}</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<header>
