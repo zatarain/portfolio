3c6daa999bfbb7f0b0fe4205cc4c2acc280db09c
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-06-21 17:40:56 +0100

    PORT-006: Adding testing for navigation bar responsive state
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- web/src/components/NavigationBar/index.spec.tsx
+++ web/src/components/NavigationBar/index.spec.tsx
@@ -1,19 +1,38 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { Provider } from 'react-redux'
+import styles from './index.module.css'
 
 import { makeStore } from '#store'
 import NavigationBar from '.'
 
 describe('<NavigationBar />', () => {
-	it('renders the component', () => {
+	it('renders the component correctly with passed name', () => {
 		const store = makeStore()
 
-		render(
+		const { asFragment, getByText } = render(
 			<Provider store={store}>
 				<NavigationBar name="My Name" />
 			</Provider>
 		)
 
-		expect(screen.getByText('My Name')).toBeInTheDocument()
+		expect(getByText('My Name')).toBeInTheDocument()
+		expect(asFragment()).toMatchSnapshot()
+	})
+
+	it('adds class name "responsive" when user clicks menu icon', async () => {
+		const store = makeStore()
+		const user = userEvent.setup()
+
+		const { container } = render(
+			<Provider store={store}>
+				<NavigationBar name="Test Name" />
+			</Provider>
+		)
+		const dropdown = container.querySelector(`li.${styles.icon} a`)
+		expect(dropdown).not.toBeNull()
+		const bar = container.querySelector('nav')
+		await user.click(dropdown)
+		expect(bar).toHaveClass(styles.responsive)
 	})
 })
