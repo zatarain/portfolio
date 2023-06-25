daa65b5d52b83c322b07de69e11432db1a0ddfd8
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-06-26 00:10:01 +0100

    PORT-007: Displaying Curriculum Vitae (#7)
    ## 🧑‍💻 Description
    These changes include the documentation for the initial requirements and
    the initial architecture diagram of the web application.
    
    ## ✅ Testing
    Confirm that the document renders correctly in GitHub website and the
    links work correctly.
    
    ## 🖼️ Evidence
    Following screenshot shows the document rendered on GitHub website:
    
    ![image](https://github.com/zatarain/portfolio/assets/539783/930b34b2-d5af-42c8-965f-35775372b0e6)
    
    ---------
    
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- web/src/components/PageLayout/index.tsx
+++ web/src/components/PageLayout/index.tsx
@@ -1,12 +1,16 @@
-import NavigationBar from '#components/NavigationBar';
+import Header from '#components/Header';
 import Head from 'next/head';
 import { ReactNode } from 'react';
 import { Inter } from 'next/font/google'
 import Footer from '#components/Footer';
 
+import styles from './index.module.css'
+
 interface Properties {
 	children?: ReactNode
-	data: any
+	data: {
+		name: string
+	}
 	title?: string
 }
 
@@ -15,13 +19,11 @@ const inter = Inter({ subsets: ['latin'] })
 export default function PageLayout({ children, title, data }: Properties) {
 	const pageTitle = [data?.name, title].filter(part => !!part).join(' - ')
 	return (
-		<div className={`${inter.className} container`}>
+		<div className={`${inter.className} ${styles.container}`}>
 			<Head>
 				<title>{pageTitle}</title>
 			</Head>
-			<header>
-				<NavigationBar name={data?.name} />
-			</header>
+			<Header data={data} />
 			<main>
 				{children}
 			</main>
