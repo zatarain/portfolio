730a660015e57dcbf82174cf93aac3ac75524219
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-06-28 07:32:18 +0100

    PORT-007: Getting credentials from environment only when we are in development
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- api/app/controllers/pages_controller.rb
+++ api/app/controllers/pages_controller.rb
@@ -5,12 +5,16 @@ require 'safe_yaml'
 
 class PagesController < ApplicationController
   def home
-    downloader = Aws::AssumeRoleCredentials.new(
-      client: Aws::STS::Client.new,
-      role_arn: Rails.configuration.aws[:assume_role],
-      role_session_name: Rails.configuration.aws[:session_name],
-    )
-    s3 = Aws::S3::Client.new(credentials: downloader)
+    if ENV['AWS_ASSUME_ROLE'].present? then
+      downloader = Aws::AssumeRoleCredentials.new(
+        client: Aws::STS::Client.new,
+        role_arn: Rails.configuration.aws[:assume_role],
+        role_session_name: Rails.configuration.aws[:session_name],
+      )
+      s3 = Aws::S3::Client.new(credentials: downloader)
+    else
+      s3 = Aws::S3::Client.new
+    end
     File.open(Rails.configuration.curriculum, 'wb') do |file|
       response = s3.get_object(
         {
@@ -19,11 +23,9 @@ class PagesController < ApplicationController
         },
         target: file,
       )
-      Rails.logger.debug 'File content: ', response.body
       file.close
     end
     cv = YAML.load(File.read(Rails.configuration.curriculum), safe: true)
-    Rails.logger.debug 'File hash: ', cv
     render json: cv, status: :ok
   end
 end
