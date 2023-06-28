7d4f7b3390333175f4289f8851bf837d8d2f7688
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-06-28 23:22:19 +0100

    PORT-007: Removing sensitive data from Curriculum by default
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- api/app/controllers/pages_controller.rb
+++ api/app/controllers/pages_controller.rb
@@ -4,28 +4,12 @@ require 'aws-sdk-s3'
 require 'safe_yaml'
 
 class PagesController < ApplicationController
+  def initialize
+    @curriculum = Curriculum.new
+  end
+
   def home
-    downloader = if ENV['AWS_ASSUME_ROLE'].present?
-                   Aws::AssumeRoleCredentials.new(
-                     client: Aws::STS::Client.new,
-                     role_arn: Rails.configuration.aws[:assume_role],
-                     role_session_name: Rails.configuration.aws[:session_name],
-                   )
-                 else
-                   Aws::ECSCredentials.new(retries: 3)
-                 end
-    s3 = Aws::S3::Client.new(credentials: downloader)
-    File.open(Rails.configuration.curriculum, 'wb') do |file|
-      s3.get_object(
-        {
-          bucket: Rails.configuration.aws[:s3_bucket],
-          key: Rails.configuration.aws[:s3_object],
-        },
-        target: file,
-      )
-      file.close
-    end
-    cv = YAML.load(File.read(Rails.configuration.curriculum), safe: true)
+    cv = @curriculum.find
     render json: cv, status: :ok
   end
 end
---
+++ api/app/models/curriculum.rb
@@ -0,0 +1,35 @@
+class Curriculum
+	include ActiveModel::Model
+
+	def initialize
+		downloader = if ENV['AWS_ASSUME_ROLE'].present?
+			Aws::AssumeRoleCredentials.new(
+				client: Aws::STS::Client.new,
+				role_arn: Rails.configuration.aws[:assume_role],
+				role_session_name: Rails.configuration.aws[:session_name],
+			)
+		else
+			Aws::ECSCredentials.new(retries: 3)
+		end
+		@s3 = Aws::S3::Client.new(credentials: downloader)
+	end
+
+	def find(sensitive = false)
+    File.open(Rails.configuration.curriculum, 'wb') do |file|
+      @s3.get_object(
+        {
+          bucket: Rails.configuration.aws[:s3_bucket],
+          key: Rails.configuration.aws[:s3_object],
+        },
+        target: file,
+      )
+      file.close
+    end
+    cv = YAML.load(File.read(Rails.configuration.curriculum), safe: true)
+		if !sensitive
+			cv.delete('phone')
+			cv['social'] = cv['social'].select {|contact| !contact['sensitive']}
+		end
+		return cv
+	end
+end
