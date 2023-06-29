2a9fd42dd286ca3e082d4451ef460fdc80f90c26
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-06-29 10:51:46 +0100

    PORT-007: Adding AWS region to all the tests cases
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- api/config/environments/test.rb
+++ api/config/environments/test.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # Setting AWS region for tests
+  ENV['AWS_REGION'] = 'eu-west-1'
 end
