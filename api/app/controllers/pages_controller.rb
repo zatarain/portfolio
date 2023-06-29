05935637dc6a81ea1d8e74b4242ca4265789b957
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-06-29 03:21:58 +0100

    PORT-006: Moving require statements to model
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- api/app/controllers/pages_controller.rb
+++ api/app/controllers/pages_controller.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-require 'aws-sdk-s3'
-require 'safe_yaml'
-
 class PagesController < ApplicationController
   def home
     @curriculum = Curriculum.new
--- api/app/models/curriculum.rb
+++ api/app/models/curriculum.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'aws-sdk-s3'
+require 'safe_yaml'
+
 class Curriculum
   include ActiveModel::Model
 
