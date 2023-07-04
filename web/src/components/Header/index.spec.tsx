3a8d62f94617708627b5fd39fab5aeb9687f0cf3
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-07-04 01:14:09 +0100

    PORT-009: Fixing unit test for Header component
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- web/src/components/Header/index.spec.tsx
+++ web/src/components/Header/index.spec.tsx
@@ -6,13 +6,12 @@ import Header from '.'
 describe('<Header />', () => {
 	it('renders the component correctly with passed name', () => {
 
-		const { asFragment, getByText } = render(
+		const { asFragment } = render(
 			<Provider store={store}>
 				<Header data={{ name: 'My Name' }} />
 			</Provider>
 		)
 
-		expect(getByText('My Name')).toBeInTheDocument()
 		expect(asFragment()).toMatchSnapshot()
 	})
 })
