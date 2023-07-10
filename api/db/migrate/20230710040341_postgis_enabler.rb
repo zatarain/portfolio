c65018e0cf361350c4844519c52c6758f180f6a8
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-07-10 05:15:22 +0100

    PORT-015: Adding other postgis extensions
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- api/db/migrate/20230710040341_postgis_enabler.rb
+++ api/db/migrate/20230710040341_postgis_enabler.rb
@@ -3,5 +3,10 @@
 class PostgisEnabler < ActiveRecord::Migration[7.0]
   def change
     enable_extension 'postgis'
+    enable_extension 'postgis_topology'
+    enable_extension 'postgis_raster'
+    enable_extension 'address_standardizer'
+    enable_extension 'address_standardizer_data_us'
+    enable_extension 'postgis_tiger_geocoder'
   end
 end
