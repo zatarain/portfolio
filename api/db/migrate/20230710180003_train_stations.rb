d573b24bad627b751f0fdd35e7a05e96cdef384d
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-07-10 21:00:18 +0100

    PORT-015: Linting migration
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- api/db/migrate/20230710180003_train_stations.rb
+++ api/db/migrate/20230710180003_train_stations.rb
@@ -1,29 +1,30 @@
+# frozen_string_literal: true
+
 class TrainStations < ActiveRecord::Migration[7.0]
   def change
     create_table :train_stations, id: :serial, force: :cascade do |column|
       column.integer :trainline_id
       column.string :name, limit: 255
       column.string :slug, limit: 63
-      column.float :latitude, null: false
-      column.float :longitude, null: false
       column.st_point :location, geographic: true, srid: 4326
       column.integer :parent_station_id
       column.string :country, limit: 2
       column.string :time_zone, limit: 64
-      column.boolean :is_city
-      column.boolean :is_main_station
-      column.boolean :is_airport
-      column.boolean :is_suggestable
-      column.boolean :country_hint
-      column.boolean :main_station_hint
+      column.boolean :is_city, default: false, null: false
+      column.boolean :is_main_station, default: false, null: false
+      column.boolean :is_airport, default: false, null: false
+      column.boolean :is_suggestable, default: false, null: false
+      column.boolean :country_hint, default: false, null: false
+      column.boolean :main_station_hint, default: false, null: false
       column.integer :same_as
       column.text :info_en
       column.text :info_es
       column.string :normalised_code, limit: 40
       column.string :iata_airport_code, limit: 3
+      column.datetime :created_at, default: -> { 'CURRENT_TIMESTAMP' }
+      column.datetime :updated_at, default: -> { 'CURRENT_TIMESTAMP' }
 
       column.index :name
-      column.index %i[latitude longitude]
       column.index :location, using: :gist
       column.index :country
       column.index :time_zone
