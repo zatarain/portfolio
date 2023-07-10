1aa26a8e4ed48a768935b99b71e92561361f9537
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-07-10 02:00:42 +0100

    PORT-015: Enabling PostGIS extensions
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

---
+++ api/db/migrate/20230710002406_postgis_enabler.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class PostgisEnabler < ActiveRecord::Migration[7.0]
+  def up
+    enable_extension 'fuzzystrmatch'
+    enable_extension 'postgis'
+    enable_extension 'postgis_raster'
+    enable_extension 'postgis_topology'
+    enable_extension 'address_standardizer'
+    enable_extension 'address_standardizer_data_us'
+    enable_extension 'postgis_tiger_geocoder'
+  end
+end
