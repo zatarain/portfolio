ee02f5d928490a2db3e80ce673e7e2ebb36c86e4
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-07-10 04:57:22 +0100

    PORT-015: Updating migration to enable single extension
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- api/db/migrate/20230710002406_postgis_enabler.rb
+++ api/db/migrate/20230710002406_postgis_enabler.rb
@@ -3,7 +3,7 @@
 class PostgisEnabler < ActiveRecord::Migration[7.0]
   def change
     enable_extension 'fuzzystrmatch'
-    enable_extension 'postgis'
+    # enable_extension 'postgis'
     # enable_extension 'postgis_topology'
     # enable_extension 'postgis_raster'
     # enable_extension 'address_standardizer'
