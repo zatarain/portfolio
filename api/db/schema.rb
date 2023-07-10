d93be4a4ea390be5f2440ae90b10163ee5ae0045
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-07-10 05:37:42 +0100

    POST-015: Ignoring all tables from PostGIS extensions
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,8 +10,51 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_10_040341) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "address_standardizer"
+  enable_extension "address_standardizer_data_us"
+  enable_extension "fuzzystrmatch"
   enable_extension "plpgsql"
+  enable_extension "postgis"
+  enable_extension "postgis_raster"
+  enable_extension "postgis_tiger_geocoder"
+  enable_extension "postgis_topology"
+
+# Could not dump table "addrfeat" because of following StandardError
+#   Unknown type 'geometry' for column 'the_geom'
+
+# Could not dump table "bg" because of following StandardError
+#   Unknown type 'geometry' for column 'the_geom'
+
+# Could not dump table "county" because of following StandardError
+#   Unknown type 'geometry' for column 'the_geom'
+
+# Could not dump table "cousub" because of following StandardError
+#   Unknown type 'geometry' for column 'the_geom'
+
+# Could not dump table "edges" because of following StandardError
+#   Unknown type 'geometry' for column 'the_geom'
+
+# Could not dump table "faces" because of following StandardError
+#   Unknown type 'geometry' for column 'the_geom'
+
+# Could not dump table "place" because of following StandardError
+#   Unknown type 'geometry' for column 'the_geom'
+
+# Could not dump table "state" because of following StandardError
+#   Unknown type 'geometry' for column 'the_geom'
+
+# Could not dump table "tabblock" because of following StandardError
+#   Unknown type 'geometry' for column 'the_geom'
+
+# Could not dump table "tabblock20" because of following StandardError
+#   Unknown type 'geometry(MultiPolygon,4269)' for column 'the_geom'
+
+# Could not dump table "tract" because of following StandardError
+#   Unknown type 'geometry' for column 'the_geom'
+
+# Could not dump table "zcta5" because of following StandardError
+#   Unknown type 'geometry' for column 'the_geom'
 
 end
