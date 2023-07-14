ec6dc1e08a4e75f0889dd583d0a3dc6bcdbcefe0
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-07-14 01:03:39 +0100

    PORT-016: Adding unit testing for model train stations
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- api/spec/models/train_station_spec.rb
+++ api/spec/models/train_station_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 describe TrainStation, type: :model do
   context 'when all fields are valid' do
     it 'save the station with generated location in the datbase' do
-      station = described_class.create!(
+      station = described_class.create(
         name: 'Canary Wharf',
         slug: 'canary-wharf',
         country: 'GB',
@@ -20,6 +20,51 @@ describe TrainStation, type: :model do
       expect(station.location).to be_present
       expect(station.location.x).to eq(-0.01861)
       expect(station.location.y).to eq(51.50361)
+    end
+  end
+
+  context 'when there are invalid fields' do
+    it 'does NOT save when the name is empty' do
+      station = described_class.create(
+        name: '',
+        slug: 'canary-wharf',
+        country: 'GB',
+        time_zone: 'Europe/London',
+        latitude: 51.50361,
+        longitude: -0.01861,
+        info_en: 'London Underground Station',
+        info_es: 'Estación del Metro de Londres',
+      )
+
+      expect(station.id).not_to be_present
+    end
+
+    it 'does NOT save when the latitude is not present' do
+      station = described_class.create(
+        name: 'Canary Wharf',
+        slug: 'canary-wharf',
+        country: 'GB',
+        time_zone: 'Europe/London',
+        longitude: -0.01861,
+        info_en: 'London Underground Station',
+        info_es: 'Estación del Metro de Londres',
+      )
+
+      expect(station.id).not_to be_present
+    end
+
+    it 'does NOT save when the longitude is not present' do
+      station = described_class.create(
+        name: 'Canary Wharf',
+        slug: 'canary-wharf',
+        country: 'GB',
+        time_zone: 'Europe/London',
+        latitude: -0.01861,
+        info_en: 'London Underground Station',
+        info_es: 'Estación del Metro de Londres',
+      )
+
+      expect(station.id).not_to be_present
     end
   end
 end
