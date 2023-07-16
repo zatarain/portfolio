65af117a0d391722b4b201d92d08d0f6f41707f1
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-07-16 04:30:18 +0100

    PORT-018: Testing stations controller when find fail
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- api/spec/requests/stations_spec.rb
+++ api/spec/requests/stations_spec.rb
@@ -32,11 +32,28 @@ RSpec.describe 'Stations' do
       allow(station).to receive(:point) { |latitude, longitude| [latitude, longitude] }
       allow(TrainStation).to receive(:new).and_return(station)
       allow(TrainStation).to receive(:within_box).and_return(data)
+
       get '/stations'
+
       expect(response).to have_http_status(:ok)
       expect(response.body).to eq(data.to_json)
       expect(TrainStation).to  have_received(:within_box)
         .with([49.000000, -7.0000], [60.000000, 7.00000])
+    end
+
+    it 'logs error message and responses with HTTP 500 on error finding the data' do
+      station = instance_double(TrainStation)
+      allow(station).to receive(:point) { |latitude, longitude| [latitude, longitude] }
+      allow(TrainStation).to receive(:within_box)
+        .and_raise(StandardError.new('Unable to find the data for train stations'))
+      allow(TrainStation).to receive(:new).and_return(station)
+      allow(Rails.logger).to receive(:error)
+
+      get '/stations'
+
+      expect(response).to have_http_status(:internal_server_error)
+      expect(Rails.logger).to have_received(:error)
+        .with(/Unable to find the data for train stations/)
     end
   end
 end
