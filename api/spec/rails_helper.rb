6c919c7d0f2175b3c118309bd349da9a9ddfebcb
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-07-16 03:35:13 +0100

    PORT-018: Testing TrainStation#within_box
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- api/spec/rails_helper.rb
+++ api/spec/rails_helper.rb
@@ -62,4 +62,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Seed database before each suite
+  config.before(:suite) do
+    Rails.application.load_seed # loading seeds
+  end
 end
