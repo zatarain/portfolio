9baa4fc3d069b460d9ff3c46d238834577d43c76
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-07-16 02:24:55 +0100

    PORT-018: Adding happy path unit test for request /stations
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- api/spec/requests/stations_spec.rb
+++ api/spec/requests/stations_spec.rb
@@ -4,6 +4,40 @@ require 'rails_helper'
 
 RSpec.describe 'Stations' do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    data = [
+      {
+        id: 1,
+        name: 'Canary Wharf',
+        slug: 'canary-wharf',
+        country: 'GB',
+        time_zone: 'Europe/London',
+        latitude: 51.50361,
+        longitude: -0.01861,
+        info_en: 'London Underground Station',
+      },
+      {
+        id: 2,
+        name: 'North Greenwich',
+        slug: 'north-greenwich',
+        country: 'GB',
+        time_zone: 'Europe/London',
+        latitude: 51.50037,
+        longitude: 0.00433,
+        info_en: 'London Underground Station',
+      },
+    ]
+
+    it 'responses with JSON data on body describing the spatial layer' do
+      station = instance_double(TrainStation)
+      allow(station).to receive(:point) { |latitude, longitude| [latitude, longitude] }
+      allow(TrainStation).to receive(:new).and_return(station)
+      allow(TrainStation).to receive(:within_box).and_return(data)
+      get '/stations'
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to eq(data.to_json)
+      expect(TrainStation).to  have_received(:within_box)
+        .with([49.000000, -7.0000], [60.000000,  7.00000])
+    end
+
   end
 end
