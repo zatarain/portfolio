0aa5720e81dcc3f4615c22fef30ec38ff96e34c8
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2026-03-20 23:48:22 +0000

    PORT-015: Removing more heredoc syntax in the scripts
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- nomad/scripts/postgres-init.sh
+++ nomad/scripts/postgres-init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 PG_VERSION="14"
 PG_BIN="/usr/local/bin"
 PG_DATA="{{ env "NOMAD_HOST_MOUNT_db_data" }}/pgdata"
@@ -17,30 +18,14 @@ if [ ! -d "$PG_DATA" ]; then
   su - postgres -c "${PG_BIN}/initdb -D $PG_DATA"
 
   # Configure PostgreSQL
-  cat >> "$PG_DATA/postgresql.conf" <<EOF
-listen_addresses = '0.0.0.0'
-port = 5432
-max_connections = 100
-shared_buffers = 256MB
-effective_cache_size = 1GB
-maintenance_work_mem = 64MB
-checkpoint_completion_target = 0.9
-wal_buffers = 16MB
-default_statistics_target = 100
-random_page_cost = 1.1
-effective_io_concurrency = 200
-work_mem = 6600kB
-min_wal_size = 1GB
-max_wal_size = 4GB
-EOF
+  cp "${SCRIPT_DIR}/templates/postgresql.conf" "$PG_DATA/postgresql.conf"
+  chown postgres:postgres "$PG_DATA/postgresql.conf"
+  chmod 640 "$PG_DATA/postgresql.conf"
 
   # Allow connections from anywhere
-  cat > "$PG_DATA/pg_hba.conf" <<EOF
-local   all             all                                     trust
-host    all             all             127.0.0.1/32            md5
-host    all             all             ::1/128                 md5
-host    all             all             0.0.0.0/0               md5
-EOF
+  cp "${SCRIPT_DIR}/templates/pg_hba.conf" "$PG_DATA/pg_hba.conf"
+  chown postgres:postgres "$PG_DATA/pg_hba.conf"
+  chmod 640 "$PG_DATA/pg_hba.conf"
 
   # Create portfolio user and database
   su - postgres -c "postgres -D $PG_DATA" &
