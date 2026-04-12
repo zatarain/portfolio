3f631a0365f5d0375a08933c767edf4bbe035118
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2026-04-12 11:36:32 +0100

    Updating storybook
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- web/.storybook/main.ts
+++ web/.storybook/main.ts
@@ -1,22 +1,25 @@
-import * as path from 'path'
+// This file has been automatically migrated to valid ESM format by Storybook.
+import { fileURLToPath } from "node:url";
+import * as path, { dirname } from 'path';
 import type { StorybookConfig } from '@storybook/nextjs'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
-    '@storybook/addon-interactions',
+    '@storybook/addon-docs'
   ],
+
   framework: {
     name: '@storybook/nextjs',
     options: {
       nextConfigPath: path.resolve(__dirname, '../next.config.js'),
     },
-  },
-  docs: {
-    autodocs: 'tag'
   }
 };
 export default config;
--- web/src/components/Markdown/index.stories.ts
+++ web/src/components/Markdown/index.stories.ts
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/nextjs'
 import Markdown from './index'
 import '#styles/math.css'
 
