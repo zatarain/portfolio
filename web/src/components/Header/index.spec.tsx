693b86a8d9c4e0e52442659a5ac4f4fafc4b57cc
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2026-04-15 16:53:57 +0100

    PORT-016: Updating unit tests and snapshots
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- web/src/components/Header/index.spec.tsx
+++ web/src/components/Header/index.spec.tsx
@@ -4,7 +4,33 @@ import { render } from '@testing-library/react'
 import Header from '.'
 
 describe('<Header />', () => {
-	it('renders the component correctly with passed name', () => {
+	it('renders the component correctly with passed name and pictures', () => {
+
+		const { asFragment } = render(
+			<Provider store={store}>
+				<Header data={{ name: 'My Name', pictures: [{ id: 'test', media_url: 'test.jpg', caption: 'Test Caption' }] }} />
+			</Provider>
+		)
+
+		expect(asFragment()).toMatchSnapshot()
+	})
+})
+
+describe('<Header />', () => {
+	it('renders the component correctly with passed name, but no pictures', () => {
+
+		const { asFragment } = render(
+			<Provider store={store}>
+				<Header data={{ name: 'My Name', pictures: [] }} />
+			</Provider>
+		)
+
+		expect(asFragment()).toMatchSnapshot()
+	})
+})
+
+describe('<Header />', () => {
+	it('renders the component correctly with passed name and not-defined pictures', () => {
 
 		const { asFragment } = render(
 			<Provider store={store}>
